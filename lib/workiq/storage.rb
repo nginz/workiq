b7c4698f8c59df90b2dd4d7600be487837e45ff8
Moustafa Badawy <moustafa@rubikal.com>
2012-11-23 22:09:59 +0200

    Storage class methods

--- lib/workiq/storage.rb
+++ lib/workiq/storage.rb
@@ -1,20 +1,22 @@
 module Workiq
   DEFAULT_EXPIRY = 60 * 30
   class Storage
-    def store_field(jid, field)
-      Sidekiq.redis do |conn|
-        result = conn.multi do
-          conn.hmset jid, *(field.to_a.flatten)
-          conn.expire jid, Workiq::DEFAULT_EXPIRY
-          conn.publish "#{jid}-field-update", jid
+    class << self
+      def store_field(jid, field)
+        Sidekiq.redis do |conn|
+          result = conn.multi do
+            conn.hmset jid, *(field.to_a.flatten)
+            conn.expire jid, Workiq::DEFAULT_EXPIRY
+            conn.publish "#{jid}-field-update", jid
+          end
+          result[0]
         end
-        result[0]
       end
-    end
 
-    def read_field(jid, field)
-      Sidekiq.redis do |conn|
-        conn.hmget(jid, field)[0]
+      def read_field(jid, field)
+        Sidekiq.redis do |conn|
+          conn.hmget(jid, field)[0]
+        end
       end
     end
   end
