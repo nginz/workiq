77a969b7ab95f469c60d14aa4ef0c9093c7aed2d
Moustafa Badawy <moustafa@rubikal.com>
2012-11-24 00:05:31 +0200

    Formatting

--- lib/workiq.rb
+++ lib/workiq.rb
@@ -3,6 +3,7 @@ require "workiq/storage"
 require "workiq/middleware/client"
 require "workiq/middleware/server"
 require "workiq/middleware/chain"
+
 module Workiq
 	def self.status(jid)
     Workiq::Storage.read_field(jid, :status).to_sym
